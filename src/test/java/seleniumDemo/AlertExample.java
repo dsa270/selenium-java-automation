dc108c9095f4ccd79f59c3b1816b151eb2c6a57a
ritika <rit@ritikas-MacBook-Air.local>
2026-03-16 23:56:41 +0530

    Added selenium alert test and login test scripts

---
+++ src/test/java/seleniumDemo/AlertExample.java
@@ -0,0 +1,19 @@
+package seleniumDemo;
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class AlertExample {
+    public static void main(String[] args) throws InterruptedException {
+
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://demoqa.com/alerts");
+        driver.findElement(By.xpath("//button[@id='alertButton']")).click();
+        Thread.sleep(3000);
+        Alert alert = driver.switchTo().alert();
+        alert.accept();
+        Thread.sleep(3000);
+        driver.quit();
+    }
+}
